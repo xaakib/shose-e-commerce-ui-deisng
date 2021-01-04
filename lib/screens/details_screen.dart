ca6829338c87589357aa1431f4f71641fd209ea6
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-05 05:33:46 +0600

    working on details screen

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatefulWidget {
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
