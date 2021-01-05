1299fd814d4c06b4261301775d34ff1fd55724b2
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-05 23:57:32 +0600

    done

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce_mobile_app/models/shoe_model.dart';
+import 'package:e_commerce_mobile_app/widgets/app_clipper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class DetailsScreen extends StatefulWidget {
   final ShoeModel shoeModel;
@@ -15,6 +17,61 @@ class _DetailsScreenState extends State<DetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: widget.shoeModel.color,
+      appBar: AppBar(
+        title: Text("Categories"),
+        elevation: 0,
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * .8,
+              child: ClipPath(
+                clipper: AppClipper(cornerSize: 50, diagonalHeight: 200),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 180),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 300,
+                          child: Text(
+                            "${widget.shoeModel.name}",
+                            style: TextStyle(
+                              fontSize: 32,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            RatingBar(
+                              initialRating: 3,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              onRatingUpdate: (rating) {
+                                print(rating);
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
