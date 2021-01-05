59c2614d0e9fc2a4b1f1e5b319d0393275aaa623
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-06 00:00:03 +0600

    ratingber are done

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -51,16 +51,22 @@ class _DetailsScreenState extends State<DetailsScreen> {
                         ),
                         Row(
                           children: [
-                            RatingBar(
+                            RatingBar.builder(
                               initialRating: 3,
                               minRating: 1,
                               direction: Axis.horizontal,
                               allowHalfRating: true,
                               itemCount: 5,
+                              itemPadding:
+                                  EdgeInsets.symmetric(horizontal: 4.0),
+                              itemBuilder: (context, _) => Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
                               onRatingUpdate: (rating) {
                                 print(rating);
                               },
-                            ),
+                            )
                           ],
                         )
                       ],
